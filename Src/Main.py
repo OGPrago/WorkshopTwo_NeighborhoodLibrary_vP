ca946c793d35feec015c41e31f65e45c641c01a2
Jeremy <ogprago@gmail.com>
2024-07-22 19:40:42 -0400

    Created bookInventory array

---
+++ Src/Main.py
@@ -0,0 +1,33 @@
+from Book import Book
+
+def main():
+    bookInventory = [
+        Book(1, "1435158695", "The Art of War", True, "Tony"),
+        Book(2, "1590309847", "The Book of Five Rings", True, "Jeremy"),
+        Book(3, "00553418033", "Fast Food Maniac", True, "Jeremy"),
+        Book(4, "1975314212", "Three Days of Happiness", True, "Jeremy"),
+        Book(5, "9781975303129", "86—EIGHTY-SIX, Vol. 1", False, ""),
+        Book(6, "9781250866448", "Friends, Lovers, and the Big Terrible Thing", False, ""),
+        Book(7, "9781974734641", "Look Back", True, "Jeremy"),
+        Book(8, "9781646512492", "A Silent Voice Complete Collector's Edition 1", True, "Jeremy"),
+        Book(9, "9781646514069", "A Silent Voice Complete Collector's Edition 2", True, "Jeremy"),
+        Book(10, "9781632366436", "A Silent Voice Complete Series Box Set", True, ""),
+        Book(11, "9781637742617", "Tremendous", True, "Jeremy"),
+        Book(12, "9780399592096", "The Ride of a Lifetime: Lessons Learned from 15 Years as CEO of the Walt Disney Company", True, "Jeremy"),
+        Book(13, "9780525478812", "The Fault in Our Stars", True, "Jeremy"),
+        Book(14, "9780765367297", "Halo: The Fall of Reach", True, "Jeremy"),
+        Book(15, "9781789092615", "Gears of War: Ascendance", True, "Jeremy"),
+        Book(16, "9781421561325", "Uzumaki", True, "Jeremy"),
+        Book(17, "9781645059998", "My Alcoholic Escape from Reality", True, "Jeremy"),
+        Book(18, "9780316254205", "Kingdom Hearts: Final Mix, Vol. 1 - manga", True, "Jeremy"),
+        Book(19, "9780316254212", "Kingdom Hearts: Final Mix Vol. 2", True, "Jeremy"),
+        Book(20, "9781501194290", "Howard Stern Comes Again", False, ""),
+        Book(21, "9780316512442", "The Saga of Tanya the Evil, Vol. 1: Deus lo Vult", True, "Jeremy"),
+        Book(22, "9780316512466", "The Saga of Tanya the Evil, Vol. 2: Plus Ultra", True, "Jeremy"),
+        Book(23, "9780316512480", "The Saga of Tanya the Evil, Vol. 3: The Finest Hour", True, "Jeremy"),
+        Book(24, "9780316560627", "The Saga of Tanya the Evil, Vol. 4: Dabit Deus his Quoque Finem", False, ""),
+        Book(25, "9781250183866", "Extreme Ownership: How U.S. Navy SEALs Lead and Win", True, "Jeremy"),
+        Book(26, "1451627289", "11/22/63: A Novel", False, ""),
+        Book(27, "1982173610", "Billy Summers", False, ""),
+        Book(28, "150114426X", "The Long Walk", False, "")
+    ]
